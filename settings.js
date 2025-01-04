656c501a1f3b1050fcac942148e0bb4fe50a4925
Meumeuccxx <shihara.max@gmail.com>
2025-01-04 08:53:13 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=vuow2LxJ#pH7siZpBXJ6cIaGcQGGGTX86y-pwmj68IgGPP7fUHAw' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=OiRRECAY#aB6GJVBzgSFyTlv1vyZ1lqOvQDs_9XqecY8eKZndkwk' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello , I am alive now!!*" : process.env.ALIVE_MSG,
